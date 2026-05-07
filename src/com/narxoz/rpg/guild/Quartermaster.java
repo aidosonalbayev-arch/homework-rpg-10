12605d8fa769ffba3f2c9bc7271ab8c76f13f690
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-05-07 21:42:12 +0500

    Implement Quartermaster: receive reacts to supply and mission topics

--- src/com/narxoz/rpg/guild/Quartermaster.java
+++ src/com/narxoz/rpg/guild/Quartermaster.java
@@ -1,5 +1,7 @@
 package com.narxoz.rpg.guild;
 
+import java.util.List;
+
 /**
  * Guild officer responsible for gear, supplies, and rewards.
  */
@@ -9,13 +11,30 @@ public class Quartermaster extends GuildMember {
         super(name, mediator);
     }
 
+    @Override
+    public List<String> getSubscribedTopics() {
+        return List.of("SUPPLY_REQUEST", "MISSION_ASSIGNED", "REWARD_CLAIMED");
+    }
+
     public void requestSupplies(String topic, String payload) {
-        // TODO: send a supply-related message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        switch (topic) {
+            case "MISSION_ASSIGNED":
+                System.out.println("[Quartermaster:" + getName() + "] Packing supplies for mission: " + payload);
+                getMediator().dispatch("SUPPLY_REQUEST", this, "Rations and gear for: " + payload);
+                break;
+            case "SUPPLY_REQUEST":
+                System.out.println("[Quartermaster:" + getName() + "] Supply request from " + from.getName() + ": " + payload);
+                break;
+            case "REWARD_CLAIMED":
+                System.out.println("[Quartermaster:" + getName() + "] Logging reward from " + from.getName() + ": " + payload);
+                break;
+            default:
+                System.out.println("[Quartermaster:" + getName() + "] Received [" + topic + "] from " + from.getName());
+        }
     }
 }
