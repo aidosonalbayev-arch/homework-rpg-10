b5b3118862d8d4c18ab3f0ed5b934c45139466f7
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-05-07 21:42:43 +0500

    Implement Healer: receive reacts to mission and heal topics

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -1,5 +1,7 @@
 package com.narxoz.rpg.guild;
 
+import java.util.List;
+
 /**
  * Guild officer responsible for wounds, potions, and recovery plans.
  */
@@ -9,13 +11,30 @@ public class Healer extends GuildMember {
         super(name, mediator);
     }
 
+    @Override
+    public List<String> getSubscribedTopics() {
+        return List.of("MISSION_ASSIGNED", "DANGER_ALERT", "HEAL_REQUEST");
+    }
+
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        switch (topic) {
+            case "MISSION_ASSIGNED":
+                System.out.println("[Healer:" + getName() + "] Preparing healing supplies for: " + payload);
+                getMediator().dispatch("HEAL_REQUEST", this, "Potions and bandages packed for " + payload);
+                break;
+            case "DANGER_ALERT":
+                System.out.println("[Healer:" + getName() + "] Alert from " + from.getName() + "! Standing by for casualties: " + payload);
+                break;
+            case "HEAL_REQUEST":
+                System.out.println("[Healer:" + getName() + "] Healing request from " + from.getName() + ": " + payload);
+                break;
+            default:
+                System.out.println("[Healer:" + getName() + "] Received [" + topic + "] from " + from.getName());
+        }
     }
 }
