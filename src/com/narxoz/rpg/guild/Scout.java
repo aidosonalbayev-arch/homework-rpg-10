515440940d6bd3747eb30fae3970594784073f7f
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-05-07 21:42:29 +0500

    Implement Scout: receive reacts to route and danger topics

--- src/com/narxoz/rpg/guild/Scout.java
+++ src/com/narxoz/rpg/guild/Scout.java
@@ -1,5 +1,7 @@
 package com.narxoz.rpg.guild;
 
+import java.util.List;
+
 /**
  * Guild officer responsible for route reports and reconnaissance.
  */
@@ -9,13 +11,30 @@ public class Scout extends GuildMember {
         super(name, mediator);
     }
 
+    @Override
+    public List<String> getSubscribedTopics() {
+        return List.of("MISSION_ASSIGNED", "ROUTE_REPORT", "DANGER_ALERT");
+    }
+
     public void reportRoute(String topic, String payload) {
-        // TODO: send a scouting message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        switch (topic) {
+            case "MISSION_ASSIGNED":
+                System.out.println("[Scout:" + getName() + "] Scouting route for: " + payload);
+                getMediator().dispatch("ROUTE_REPORT", this, "Path to " + payload + " is clear. No ambushes detected.");
+                break;
+            case "ROUTE_REPORT":
+                System.out.println("[Scout:" + getName() + "] Route intel from " + from.getName() + ": " + payload);
+                break;
+            case "DANGER_ALERT":
+                System.out.println("[Scout:" + getName() + "] DANGER from " + from.getName() + "! " + payload);
+                break;
+            default:
+                System.out.println("[Scout:" + getName() + "] Received [" + topic + "] from " + from.getName());
+        }
     }
 }
