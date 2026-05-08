08c7efd0ed4c85400224c1fda7d361aad23812ba
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-05-08 17:29:40 +0500

    Implement Captain: receive reacts to route, supply, heal, danger topics

--- src/com/narxoz/rpg/guild/Captain.java
+++ src/com/narxoz/rpg/guild/Captain.java
@@ -1,5 +1,7 @@
 package com.narxoz.rpg.guild;
 
+import java.util.List;
+
 /**
  * Guild officer responsible for orders and mission coordination.
  */
@@ -9,13 +11,32 @@ public class Captain extends GuildMember {
         super(name, mediator);
     }
 
+    @Override
+    public List<String> getSubscribedTopics() {
+        return List.of("ROUTE_REPORT", "SUPPLY_REQUEST", "HEAL_REQUEST", "DANGER_ALERT");
+    }
+
     public void issueOrder(String topic, String payload) {
-        // TODO: send a command message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        switch (topic) {
+            case "ROUTE_REPORT":
+                System.out.println("[Captain:" + getName() + "] Route confirmed by " + from.getName() + ": " + payload);
+                break;
+            case "SUPPLY_REQUEST":
+                System.out.println("[Captain:" + getName() + "] Approving supply request from " + from.getName() + ": " + payload);
+                break;
+            case "HEAL_REQUEST":
+                System.out.println("[Captain:" + getName() + "] Noted medical prep from " + from.getName() + ": " + payload);
+                break;
+            case "DANGER_ALERT":
+                System.out.println("[Captain:" + getName() + "] !! DANGER ALERT from " + from.getName() + ": " + payload + " - Ordering defensive formation!");
+                break;
+            default:
+                System.out.println("[Captain:" + getName() + "] Received [" + topic + "] from " + from.getName());
+        }
     }
 }
